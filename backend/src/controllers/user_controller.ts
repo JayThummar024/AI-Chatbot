9d1c38f9b39c54c23fd81d0d2975547a139aec88
KevalPatel005 <kevalpatelb400@gmail.com>
2024-09-25 21:37:05 +0100

    Implemented user signup functionality

--- backend/src/controllers/user_controller.ts
+++ backend/src/controllers/user_controller.ts
@@ -1,11 +1,32 @@
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 
-export const getAllUsers = async (req,res,next) => {
+export const getAllUsers = async (req, res, next) => {
   try {
     const users = await User.find();
-    return res.status(200).json({message:"Ok" , users});
+    return res.status(200).json({ message: "Ok", users });
   } catch (err) {
-    console.log(err)
-    return res.status(404).json({ message: "Error", error : err});
+    console.log(err);
+    return res.status(404).json({ message: "Error", error: err });
+  }
+};
+
+export const userSignup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const hashedPass = await hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPass,
+    });
+
+    await user.save();
+    return res.status(200).json({ message: "Ok", id: user._id.toString() });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ message: "Error", error: err });
   }
 };
--- backend/src/routes/user_routes.ts
+++ backend/src/routes/user_routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user_controller.js";
+import { getAllUsers, userSignup } from "../controllers/user_controller.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
+userRoutes.post("/signup", userSignup);
 
 export default userRoutes;
